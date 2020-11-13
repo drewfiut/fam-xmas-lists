1f788b9eeff9a0cacf37eca0e03b0b8e687efc56
Drew Fiutko <drewfiut@buffalo.edu>
2020-11-13 13:03:49 -0500

    redirect to lists, create profile page

--- fam-xmas-lists/src/app/app-routing.module.ts
+++ fam-xmas-lists/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListsComponent } from './lists/lists.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [
-  { path: '', component: ListsComponent }
+  { path: '',  redirectTo: '/lists', pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'lists', component: ListsComponent }
 ];
 
 @NgModule({
--- fam-xmas-lists/src/app/profile/profile.component.ts
+++ fam-xmas-lists/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  current;
+
+  constructor(public auth: AuthService){
+    this.current = this.auth.user$;
+  }
 
   ngOnInit(): void {
   }
